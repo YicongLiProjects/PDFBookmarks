08d9c733546b49462cffbaff5784b7f04efb10d7
Yicong Li <yicongli2005@outlook.com>
2025-09-25 22:14:53 -0400

    Add project files.

---
+++ PDFBookmarks/MainMenu.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PDFBookmarks
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            // Event handlers of UI components
+            InitializeComponent();
+            HelpButton.Click += OpenHelpMenu;
+            SettingsButton.Click += OpenSettingsMenu;
+        }
+
+        // Open a PDF file from the list box
+        public void OpenFile(object sender, MouseEventArgs e)
+        {
+            ListBoxItem? lbi = sender as ListBoxItem;
+            // Navigate to the file viewer if a file is double-clicked
+            if (lbi != null && lbi.IsSelected)
+            {
+                Uri newPage = new Uri("FileEditor.xaml", UriKind.Relative);
+                MainWindow.MainFrame?.Navigate(newPage);
+            }
+        }
+
+        public void OpenSettingsMenu(object sender, RoutedEventArgs e)
+        {
+            MainWindow.MainFrame?.Navigate(new Uri("SettingsMenu.xaml", UriKind.Relative));
+        }
+
+        public void OpenHelpMenu(object sender, RoutedEventArgs e)
+        {
+            MainWindow.MainFrame?.Navigate(new Uri("HelpMenu.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ PDFBookmarks/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PDFBookmarks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Make the frame static because it's the only frame and it needs
+        // to be accessed from other pages
+        public static Frame? MainFrame
+        {
+            get; set;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Initialize the main frame and navigate to the main menu when the app opens
+            MainFrame = AppFrame;
+            MainFrame?.Navigate(new Uri("MainMenu.xaml", UriKind.Relative));
+        }
+
+    }
+}
